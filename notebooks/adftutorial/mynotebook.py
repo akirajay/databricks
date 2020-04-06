fc8d0dadc4e70f534143b1237df2bb534d20855a
Akira_Cat <wuliangjay@gmail.com>
2020-04-07 08:49:02 +0900

    v1.2

---
+++ notebooks/adftutorial/mynotebook.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# Creating widgets for leveraging parameters, and printing the parameters
+
+dbutils.widgets.text("input", "","")
+y = dbutils.widgets.get("input")
+print ("Param -\'input':")
+print (y)
